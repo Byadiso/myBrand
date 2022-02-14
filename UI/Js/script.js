8d6a9db568c82b3f229f1243c528d870b1bfc40c
Byadiso <byadiso@gmail.com>
2022-02-14 16:13:02 +0100

    adding toggle features for nav bar

--- UI/Js/script.js
+++ UI/Js/script.js
@@ -2,12 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const burger_menu = document.querySelectorAll(".burger_menu");
   const small_nav = document.querySelectorAll(".menu_nav");
 
-  console.log("We are going to click my burger menu");
   burger_menu.forEach((button) => {
     button.addEventListener("click", (e) => {
-      console.log("click my burger menu");
       for (var i = 0; i < small_nav.length; i++) {
         small_nav[i].classList.remove("small_device");
+        button.classList.add("hide_burger");
       }
     });
   });
