3725eb84ef39a56248e8e9062a1b0537cff5b1b0
Byadiso <byadiso@gmail.com>
2022-02-14 13:37:35 +0100

    adding click event on burger nav icon

--- UI/Js/script.js
+++ UI/Js/script.js
@@ -1,1 +1,14 @@
-console.log("yes we are on");
+document.addEventListener("DOMContentLoaded", () => {
+  const burger_menu = document.querySelectorAll(".burger_menu");
+  const small_nav = document.querySelectorAll(".menu_nav");
+
+  console.log("We are going to click my burger menu");
+  burger_menu.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      console.log("click my burger menu");
+      for (var i = 0; i < small_nav.length; i++) {
+        small_nav[i].classList.remove("small_device");
+      }
+    });
+  });
+});
