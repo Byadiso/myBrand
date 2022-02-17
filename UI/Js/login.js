616be02e33e4e845b9fa673834d328fa3f626aa2
Byadiso <byadiso@gmail.com>
2022-02-17 12:28:32 +0100

    adding script file for login

--- UI/Js/login.js
+++ UI/Js/login.js
@@ -1,1 +1,24 @@
-console.log("yes we are on");
+document.addEventListener("DOMContentLoaded", () => {
+  const loginButton = document.querySelectorAll(".login");
+  const registerButton = document.querySelectorAll(".register");
+  const registerForm = document.querySelector("#register_form");
+  const loginForm = document.querySelector("#login_form");
+
+  registerButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      console.log("let bring register form");
+      registerForm.classList.remove("hide");
+      loginForm.classList.add("hide");
+    });
+  });
+
+  loginButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      console.log("let bring login form");
+      registerForm.classList.add("hide");
+      loginForm.classList.remove("hide");
+    });
+  });
+});
+
+console.log("yes we are on login page");
