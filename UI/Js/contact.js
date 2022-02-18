a6f1160a42c55f9e41836c7a134e8b14048f20dc
Byadiso <byadiso@gmail.com>
2022-02-18 02:47:26 +0100

    working on validation

--- UI/Js/contact.js
+++ UI/Js/contact.js
@@ -4,9 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const sendRequestButton = document.querySelectorAll(".send_request");
   const requestForm = document.querySelector("#request_form");
   const contactForm = document.querySelector("#contact_form");
-  const name = document.querySelector("#name");
-  const email = document.querySelector("#email");
-  const message = document.querySelector("#message");
+  const name = document.querySelector('[name="name"]');
+  const email = document.querySelector('[name="email"]');
+  const message = document.querySelector('[name="message"]');
+  console.log(name.value);
 
   contactButton.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -29,15 +30,23 @@ document.addEventListener("DOMContentLoaded", () => {
   sendRequestButton.forEach((button) => {
     button.addEventListener("click", (e) => {
       console.log("let send your request");
-      const nameInput = document.querySelector("input#name");
-      console.log(name.value);
-      //   if (!name.value || !message.value || !email.value) {
-      //     console.log("first add something");
-      //     if (!name.value) {
-      //       name.classList.add("error_message");
-      //       nameInput.style.border = "border: 1px solid red";
-      //     }
-      //   }
+
+      if (!name.value || !message.value || !email.value) {
+        // name.style.border = "1px solid red";
+        // email.style.border = "1px solid red";
+        console.log("first add something");
+        if (!name.value) {
+          name.style.border = "1px solid red";
+          //   console.log((name.style.border = "1px solid red"));
+        }
+
+        if (!email.value) {
+          email.style.border = "1px solid red";
+        }
+        if (!message.value) {
+          message.style.border = "1px solid red";
+        }
+      }
     });
   });
 });
