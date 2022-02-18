799e42265ccb667f31547174c99044296508a40c
Byadiso <byadiso@gmail.com>
2022-02-18 15:17:24 +0100

    adding-dashboard logics

--- UI/Js/login.js
+++ UI/Js/login.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const registerButton = document.querySelectorAll(".register");
   const registerForm = document.querySelector("#register_form");
   const loginForm = document.querySelector("#login_form");
+  const password = document.querySelector('[name="password"]');
+  const email = document.querySelector('[name="email"]');
+
+  console.log("yes we are on login page");
 
   registerButton.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -17,8 +21,23 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("let bring login form");
       registerForm.classList.add("hide");
       loginForm.classList.remove("hide");
+
+      if (!email.value || !password.value) {
+        console.log("first add something");
+        if (!email.value) {
+          email.style.border = "1px solid red";
+        }
+        if (!password.value) {
+          password.style.border = "1px solid red";
+          //   console.log((name.style.border = "1px solid red"));
+        }
+      }
+      if (email.value && password.value) {
+        console.log("let do rogin");
+        window.location.href = "../html/dashboard.html";
+      }
     });
   });
-});
 
-console.log("yes we are on login page");
+  // login logic
+});
