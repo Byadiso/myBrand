2450134d9efa55f867bc07f8f5810cb56b2675f3
Byadiso <byadiso@gmail.com>
2022-02-21 15:05:26 +0100

    saving data in localstorage

--- UI/Js/blog.js
+++ UI/Js/blog.js
@@ -1,4 +1,26 @@
-console.log("yes we are on");
-import blogs from "../Js/blogs";
+// save our id to localStorage
 
-console.log(blogs[0]);
+const block = document.getElementById("blog_admin");
+
+block.addEventListener("click", (e) => {
+  e.preventDefault();
+  let id = e.target.dataset.id;
+  console.log(e.target.dataset.id);
+
+  //   if (e.target.classList === "readme_button") {
+  //     console.log("yes");
+  //   }
+
+  const id_clicked = JSON.stringify(id);
+
+  // save to localStorage
+  localStorage.setItem("id", id_clicked);
+
+  //   //function to get inpu value
+
+  //   function getInputValue(id) {
+  //     return document.getElementById(id).value;
+  //   }
+
+  //   console.log("blog added successully");
+});
