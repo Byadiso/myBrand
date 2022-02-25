9c4642b95b034b1a35833369c4a786b491e5ca15
Byadiso <byadiso@gmail.com>
2022-02-25 02:46:31 +0100

    debuggin edit and delete feature on  dashboard page

--- Js/blog.js
+++ Js/blog.js
@@ -18,7 +18,7 @@ block.addEventListener("click", (e) => {
   let idSave = localStorage.setItem("id", id_clicked);
 
   if (id_clicked) {
-    window.location.href = `../html/single_blog.html/?id=${id_clicked}`;
+    window.location.href = `../html/single_blog.html`;
   }
   //   //function to get inpu value
 
