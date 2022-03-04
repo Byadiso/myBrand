98030b50176b90316aa3f706e64d069b7368c037
Byadiso <byadiso@gmail.com>
2022-03-04 11:19:08 +0100

    finalizing debuggin single post with firebase

--- server.js
+++ server.js
@@ -18,22 +18,6 @@ app.use((req, res, next) => {
   next();
 });
 
-//db
-mongoose
-  .connect(
-    process.env.MONGODB_URI ||
-      "mongodb+srv://byadiso:Uwineza3010@cluster0.kbaby.mongodb.net/kodesha?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("DB Connected"))
-  .catch((err) => {
-    console.error(`Error connecting to  the database . \n${err}`);
-  });
-
 //middlewares
 app.use(bodyParser.json());
 
